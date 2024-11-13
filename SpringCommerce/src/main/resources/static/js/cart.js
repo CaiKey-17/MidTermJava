432a43019475428e8b629863dde074f0746ca06a
CaiKey-17 <163799447+CaiKey-17@users.noreply.github.com>
2024-11-13 07:48:51 +0700

    Update cart.js

--- SpringCommerce/src/main/resources/static/js/cart.js
+++ SpringCommerce/src/main/resources/static/js/cart.js
@@ -55,8 +55,8 @@ function addToCart(button) {
 
 
 function updateCartView() {
-    var cartTable = document.querySelector("#cart-view tbody");
-    cartTable.innerHTML = "";
+    var cartContainer = document.querySelector("#cart-view tbody");
+    cartContainer.innerHTML = ""; 
 
     var total = 0;
     var totalQuantity = 0;
@@ -64,22 +64,22 @@ function updateCartView() {
     cart.forEach(function(product) {
         totalQuantity += product.quantity;
 
-        var newRow = document.createElement("tr");
-        newRow.classList.add("item_" + product.id);
-
         var color = product.variant && product.variant.color ? product.variant.color : "Màu sắc không xác định";
         var size = product.variant && product.variant.size ? product.variant.size : "Kích thước không xác định";
 
+        var newRow = document.createElement("tr");
+        newRow.classList.add("item_" + product.id);
+
         newRow.innerHTML = `
             <td class="img">
-                <a href="/products/${product.id}" title="${product.name}">
+                <a th:href="'/detail/' + ${product.id}" title="${product.name}">
                     <img src="${product.img}" alt="${product.name}">
                 </a>
             </td>
             <td>
                 <a class="pro-title-view" href="/products/${product.id}" title="${product.name}">${product.name}</a>
                 <span class="variant">${color} / ${size}</span>
-                <span class="pro-quantity-view">Số lượng: ${product.quantity}</span>
+                <span class="pro-quantity-view" onclick="increaseQuantity('${product.id}')">Số lượng: ${product.quantity}</span>
                 <span class="pro-price-view">${product.price}</span>
                 <span class="remove_link remove-cart">
                     <a href="javascript:void(0);" onclick="deleteCart('${product.id}')"><i class="fa fa-times"></i></a>
@@ -87,17 +87,23 @@ function updateCartView() {
             </td>
         `;
 
-        cartTable.appendChild(newRow);
+        cartContainer.appendChild(newRow);
 
         total += parseInt(product.price.replace(/[₫,]/g, '')) * product.quantity;
     });
 
     document.querySelector('.count').innerText = `(${totalQuantity})`;
-
     document.getElementById("total-view-cart").innerText = total.toLocaleString('vi-VN') + '₫';
 }
 
-
+function increaseQuantity(productId) {
+    var product = cart.find(item => item.id === productId);
+    if (product) {
+        product.quantity += 1;
+        updateCartView();
+        saveCartToLocalStorage(); 
+    }
+}
 function deleteCart(productId) {
     cart = cart.filter(item => item.id !== productId);
     updateCartView();
